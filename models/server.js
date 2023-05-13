34744a4a1051dbef17e5d5d69c3d8044def5e8f4
mivargas <miguelvargas619@gmail.com>
2023-05-13 16:25:00 -0400

    port default

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3200;
         this.userPath = '/api/users';
 
         //Middelewares
